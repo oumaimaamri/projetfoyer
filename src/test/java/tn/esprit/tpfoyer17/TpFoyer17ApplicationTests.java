fc103e818ba626e0067762a57be810b64d525da9
oumaimaamri <oamri1548@gmail.com>
2024-12-11 19:00:15 +0100

    update pom.xml

--- src/test/java/tn/esprit/tpfoyer17/TpFoyer17ApplicationTests.java
+++ src/test/java/tn/esprit/tpfoyer17/TpFoyer17ApplicationTests.java
@@ -4,7 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TpFoyer17ApplicationTests {
+class
+TpFoyer17ApplicationTests {
 
     @Test
     void contextLoads() {
